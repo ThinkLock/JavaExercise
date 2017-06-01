663e6df5852b6baab90a8da4107cd84bf9a535de
fengzhaoyang_i <fengzhaoyang_i@didichuxing.com>
2017-06-01 18:19:53 +0800

    面试算法实现

--- src/leetcode/_79_.java
+++ src/leetcode/_79_.java
@@ -5,4 +5,7 @@ package leetcode;
  * Created by fengzhaoyang_i on 2017/5/15.
  */
 public class _79_ {
+    public static final void main(String[] args) {
+        System.out.println("hello world" + args[0]);
+    }
 }
